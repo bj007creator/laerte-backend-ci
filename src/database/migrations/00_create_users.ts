2a2b5adb0a1f53ec36f7deec0c3d7f87c0b97790
mateus-coder1 <letsgood56@gmail.com>
2020-10-04 16:10:20 -0300

    Fix database config

--- src/database/migrations/00_create_users.ts
+++ src/database/migrations/00_create_users.ts
@@ -1,8 +1,7 @@
 import Knex from "knex";
 
 export async function up(knex: Knex) {
-  await knex.schema.dropTable("users");
-  return await knex.schema.createTable("users", table => {
+  return knex.schema.createTable("users", table => {
     table.increments("id").primary();
     table.string("name").notNullable();
     table.string("password").notNullable();
@@ -11,5 +10,5 @@ export async function up(knex: Knex) {
   });
 }
 export async function down(knex: Knex) {
-  return await knex.schema.dropTable("users");
+  return knex.schema.dropTable("users");
 }
